6b230ea648542a921ac959ed9986920d1cfed30b
prajapatshrvan <shravan@singhsoft.com>
2025-01-26 15:38:29 +0530

    holiday

--- routers/holiday.router.js
+++ routers/holiday.router.js
@@ -5,49 +5,49 @@ const router = require("express").Router();
 router.post(
   "/addbulkholiday",
   auth,
-  checkAccess("holidays", "create"),
+  // checkAccess("holidays", "create"),
   Holiday.addBulkHoliday
 );
 
 router.post(
   "/update/holiday",
   auth,
-  checkAccess("holidays", "edit"),
+  // checkAccess("holidays", "edit"),
   Holiday.UpdateHoliday
 );
 
 router.post(
   "/holiday/status",
   auth,
-  checkAccess("holidays", "edit"),
+  // checkAccess("holidays", "edit"),
   Holiday.HolidayStatus
 );
 
 router.post(
   "/deleteholiday",
   auth,
-  checkAccess("holidays", "delete"),
+  // checkAccess("holidays", "delete"),
   Holiday.deleteHoliday
 );
 
 router.get(
   "/holiday/list",
   auth,
-  checkAccess("holidays", "view"),
+  // checkAccess("holidays", "view"),
   Holiday.List
 );
 
 router.get(
   "/getholiday",
   auth,
-  checkAccess("holidays", "view"),
+  // checkAccess("holidays", "view"),
   Holiday.getholiday
 );
 
 router.post(
   "/holiday/approved",
   auth,
-  checkAccess("holidays", "edit"),
+  // checkAccess("holidays", "edit"),
   Holiday.approved_and_rejected
 );
 
